e04b0c912a2d1d605136be769b60bd41a4696707
RDXV <varunarora1408@gmail.com>
2021-11-20 19:45:51 +0530

    Day 6 and 7 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
 // ignore_for_file:prefer_const_constructors
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -24,7 +31,7 @@ class LoginPage extends StatelessWidget {
               height: 20,
             ),
             Text(
-              'Welcome',
+              'Welcome $name',
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
             SizedBox(
@@ -39,6 +46,10 @@ class LoginPage extends StatelessWidget {
                       hintText: 'Enter username',
                       labelText: 'Username',
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -50,18 +61,59 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 40,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Hi Varun. login button was pressed');
+                  // GestureDetector does not give feedback(ripple or splash effect) but inkwell gives the feedback animation
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(milliseconds: 900));
+                      // This async and await are important for the animation to show and work otherwise animation wouldn't work
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                    child: Text('Login'),
-                    // style: accepts buttonstyle but instead we use TextButton which extends from ButtonStyleButton .stylefrom() which is ButtonStyle
-                    // styleFrom and so this works rather than ButtonStyle
-                    style: TextButton.styleFrom(
-                      minimumSize: Size(150, 55),
+                    // Use Ink above Container(which became AnimatedContainer) with color defined inside Ink rather than boxdecoration for ripple effect
+                    child: AnimatedContainer(
+                      // duration parameter required
+                      // curve definition is optional
+                      duration: Duration(milliseconds: 700),
+                      alignment: Alignment.center,
+                      height: 50,
+                      width: changeButton ? 50 : 150,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // We can instead use shape rather than borderRadius like shape: which can either be rectangle or circle
+                        // shape: BoxShape.rectangle or BoxShape.circle or .values(when giving specific values)
+                        // Also note that you can use either shape or radius not both as they have contradiction
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                      ),
                     ),
-                  )
+                  ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     print('Hi Varun. login button was pressed');
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   child: Text('Login'),
+                  //   // style: accepts buttonstyle but instead we use TextButton which extends from ButtonStyleButton .stylefrom() which is ButtonStyle
+                  //   // styleFrom and so this works rather than ButtonStyle
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(150, 55),
+                  //   ),
+                  // ),
                 ],
               ),
             )
